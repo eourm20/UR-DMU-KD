16b350747cd54b335276b5b47bbe4e13e4272ba1
eourm20 <eourm20@gmail.com>
2024-11-05 16:12:03 +0900

    실험 완료

--- dataset_loader.py
+++ dataset_loader.py
@@ -70,8 +70,8 @@ class Unlabeled_UCF_crime(data.DataLoader):
         self.modal = modal
         self.num_segments = num_segments
         self.len_feature = len_feature
-        origin_split_path = os.path.join('list','KD/unlabel_75/ucf-unlabel-i3d.list')
-        weak_aug_split_path = os.path.join('list','KD/unlabel_75/ucf-unlabel-i3d_5.list')
+        origin_split_path = os.path.join('list','KD/unlabel_50/ucf-unlabel-i3d.list')
+        weak_aug_split_path = os.path.join('list','KD/unlabel_50/ucf-unlabel-i3d_5.list')
         origin_split_file = open(origin_split_path, 'r')
         weak_aug_split_file = open(weak_aug_split_path, 'r')
         self.origin_vid_list = []
